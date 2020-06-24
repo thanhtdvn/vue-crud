c4daf8f81c9d86d2650ee5cfaeee419cdc0d4bb8
thanhtdvn <truongducthanh88@gmail.com>
2020-06-24 21:12:29 +0700

    Initialize the app with CRM template

---
+++ src/config/api.js
@@ -0,0 +1,9 @@
+export default {
+//  url: 'http://127.0.0.1:8000', /* type http://crm-api.id-a.pl if you want to use demo API */
+  url: 'http://crm-api.id-a.pl',
+  path: {
+    prefix: 'api',
+    storage: 'storage',
+    upload: 'files/file-upload',
+  },
+}
